02d6fd801842129bbb10c1759265e0ad3941e2d0
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-11-23 15:15:35 -0300

    fix - adicionando plugins e imagens

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import VueRouter from 'vue-router';
 import router from './router/router'
+import './plugins/components'
+
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
