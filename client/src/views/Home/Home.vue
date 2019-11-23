26d27d007442d7c723571481bf0856bfa75970fc
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-11-23 15:12:55 -0300

    feature - atualizando views e adicionando home

---
+++ client/src/views/Home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-container>
+		<v-row>
+			<v-col 
+			v-for="i in 12"
+			:key="i"
+			col="3"
+			>
+				<card-image 
+					:programa="titulo"
+					:path="path"
+				/>
+			</v-col>
+		</v-row>
+		<span>{{titulo}}</span>
+	</v-container>
+</template>
+<style scoped>
+
+</style>
+<script src="./HomeController.js"> </script>
---
+++ client/src/views/Home/HomeController.js
@@ -0,0 +1,12 @@
+export default {
+	name: 'Home',
+	data() {
+		return {
+			titulo: 'Teste',
+			path: 'images/programas/ELP.png'
+		}
+	},
+	methods: {
+
+	}
+}
