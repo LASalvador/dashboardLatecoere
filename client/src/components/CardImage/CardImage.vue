9dfe383596597d281aec0d77ad8353ac6a4d0991
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-11-23 15:13:56 -0300

    feature - adicionando componente card image

---
+++ client/src/components/CardImage/CardImage.vue
@@ -0,0 +1,52 @@
+<template>
+	<v-card
+    class="mx-auto"
+    color="latecoereGray"
+    max-height="250"
+    to="/programa"
+  >
+    <v-img
+      class="align-end"
+      width="200"
+      :src="path"
+    >
+    </v-img>
+    <v-card-subtitle class="pb-0">Programa: {{ programa }}</v-card-subtitle>
+    <v-card-actions>
+      <v-btn
+        color="latecoereBlueDark"
+        text
+        to="/programa"
+      >
+        Visualizar
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<style scoped>
+
+</style>
+<script> 
+  export default {
+    name: 'CardImage',
+    props: {
+      programa: {
+        type: String,
+        required: true,
+      },
+      path: {
+        type: String,
+        required: true,
+      }
+
+    },
+    data (){
+      return {
+
+      }
+    },
+    methods : {
+
+    }
+  }
+</script>
---
+++ client/src/plugins/components.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import CardImage from '../components/CardImage/CardImage'
+
+Vue.component(CardImage.name, CardImage)
