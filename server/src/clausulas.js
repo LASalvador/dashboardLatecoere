3978866546d84051cf9651de226d671daf6c5f23
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-11-23 21:53:53 -0300

    fix - atualizando server

--- server/src/clausulas.js
+++ server/src/clausulas.js
@@ -6,57 +6,19 @@ const bd = new sqlite3.Database('./bdparceria.db', (error) => {
     if (error) {
         console.log(error.message);
     }
-    else {
-        console.log("BD criado");
-        // verifica se a tabela tbdados existe
-        bd.get("select name from sqlite_master where type='table' and name=?", ['tbdados'], (error, row) => {
-            if (error) {
-                console.log(error.message);
-            }
-            else if (row == undefined) {
-                bd.run(
-                    'create table if not exists tbdados(' +
-                    'range text NULL,' +
-                    'linha text NULL,' +
-                    'posto text NULL,' +
-                    'descricao text NULL,' +
-                    'datainicioplan text NULL,' +
-                    'datafimplan text NULL,' +
-                    'datainicioreal text NULL,' +
-                    'datafimreal text NULL,' +
-                    'fimreal_fimplan real NULL,' +
-                    'inicioreal_inicioplan real NULL,' +
-                    'fimreal_inicioreal real NULL,' +
-                    'fimplan_inicioplan real NULL' +
-                    ')'
-                );
-                console.log("tbdados criada");
-            }
-        });
-    }
 });
 
-// retorna todos os registros que possuem o range fornecido
-const getByRange = (request, response) => {
+// retorna o somatório e média de uma determinada linha agrupados pelo posto
+const getLinhaGroupByPosto = (request, response) => {
     bd.all(
-        'select range, linha, posto, descricao, datainicioplan, datafimplan, datainicioreal, datafimreal, fimreal_fimplan, inicioreal_inicioplan, fimreal_inicioreal, fimplan_inicioplan ' +
-        'from tbdados where range = ?',
-        [request.params.range],
-        (error, rows) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json(rows);
-        }
-    );
-};
-
-
-// retorna todos os registros que possuem a linha fornecida
-const getByLinha = (request, response) => {
-    bd.all(
-        'select range, linha, posto, descricao, datainicioplan, datafimplan, datainicioreal, datafimreal, fimreal_fimplan, inicioreal_inicioplan, fimreal_inicioreal, fimplan_inicioplan ' +
-        'from tbdados where linha = ?',
+        'select linha, posto, '+ 
+        'round(sum(fimreal_fimplan),2) as "sum_fimreal_fimplan", round(sum(inicioreal_inicioplan),2) as "sum_inicioreal_inicioplan", ' + 
+        'round(sum(fimreal_inicioreal),2) as "sum_fimreal_inicioreal", round(sum(fimplan_inicioplan),2) as "sum_fimplan_inicioplan", ' +
+        'round(avg(fimreal_fimplan),2) as "avg_fimreal_fimplan", round(avg(inicioreal_inicioplan),2) as "avg_inicioreal_inicioplan", ' + 
+        'round(avg(fimreal_inicioreal),2) as "avg_fimreal_inicioreal", round(avg(fimplan_inicioplan),2) as "avg_fimplan_inicioplan" ' +
+        'from tbdados '+
+        'where linha = ?' +
+        'group by posto',
         [request.params.linha],
         (error, rows) => {
             if (error) {
@@ -67,13 +29,18 @@ const getByLinha = (request, response) => {
     );
 };
 
-
-// retorna todos os registros que possuem o posto fornecido
-const getByPosto = (request, response) => {
+// retorna o somatório e média de uma determinada linha agrupados pelo posto e range
+const getLinhaRangeGroupByPosto = (request, response) => {
     bd.all(
-        'select range, linha, posto, descricao, datainicioplan, datafimplan, datainicioreal, datafimreal, fimreal_fimplan, inicioreal_inicioplan, fimreal_inicioreal, fimplan_inicioplan ' +
-        'from tbdados where posto = ?',
-        [request.params.posto],
+        'select linha, range, posto, '+ 
+        'round(sum(fimreal_fimplan),2) as "sum_fimreal_fimplan", round(sum(inicioreal_inicioplan),2) as "sum_inicioreal_inicioplan", ' + 
+        'round(sum(fimreal_inicioreal),2) as "sum_fimreal_inicioreal", round(sum(fimplan_inicioplan),2) as "sum_fimplan_inicioplan", ' +
+        'round(avg(fimreal_fimplan),2) as "avg_fimreal_fimplan", round(avg(inicioreal_inicioplan),2) as "avg_inicioreal_inicioplan", ' + 
+        'round(avg(fimreal_inicioreal),2) as "avg_fimreal_inicioreal", round(avg(fimplan_inicioplan),2) as "avg_fimplan_inicioplan" ' +
+        'from tbdados '+
+        'where linha = ? and range = ? ' +
+        'group by posto',
+        [request.params.linha, request.params.range],
         (error, rows) => {
             if (error) {
                 throw error;
@@ -82,43 +49,15 @@ const getByPosto = (request, response) => {
         }
     );
 };
-
-// retorna todos os registros que possuem o range e o posto fornecido
-const getByRangePosto = (request, response) => {
-    bd.all(
-        'select range, linha, posto, descricao, datainicioplan, datafimplan, datainicioreal, datafimreal, fimreal_fimplan, inicioreal_inicioplan, fimreal_inicioreal, fimplan_inicioplan ' +
-        'from tbdados where range = ? and posto = ?',
-        [request.params.range, request.params.posto],
-        (error, rows) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json(rows);
-        }
-    );
-};
-
-// retorna todos os registros que possuem o range, linha e posto fornecido
-const getByRangeLinhaPosto = (request, response) => {
-    bd.all(
-        'select range, linha, posto, descricao, datainicioplan, datafimplan, datainicioreal, datafimreal, fimreal_fimplan, inicioreal_inicioplan, fimreal_inicioreal, fimplan_inicioplan ' +
-        'from tbdados where range = ? and linha = ? and posto = ?',
-        [request.params.range, request.params.linha, request.params.posto],
-        (error, rows) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json(rows);
-        }
-    );
-};
-
-// *************************
 
 // retorna os tipos de range
-const getDistinctRange = (request, response) => {
+const getDistinctRangeByLinha = (request, response) => {
     bd.all(
-        'select distinct range from tbdados order by range',
+        'select distinct range '+
+        'from tbdados '+
+        'where linha = ? '+
+        'order by range',
+        [request.params.linha],
         (error, rows) => {
             if (error) {
                 throw error;
@@ -128,7 +67,6 @@ const getDistinctRange = (request, response) => {
     );
 };
 
-// retorna os tipos de linha
 const getDistinctLinha = (request, response) => {
     bd.all(
         'select distinct linha from tbdados order by linha',
@@ -141,135 +79,9 @@ const getDistinctLinha = (request, response) => {
     );
 };
 
-// retorna os tipos de descricao
-const getDistinctPosto = (request, response) => {
-    bd.all(
-        'select distinct posto from tbdados order by posto',
-        (error, rows) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json(rows);
-        }
-    );
-};
-
-// *************************
-
-// retorna o somatório e média dos registros que possuem o range fornecido
-const getStatsByRange = (request, response) => {
-    bd.all(
-        'select range, '+ 
-        'round(sum(fimreal_fimplan),2) as "sum_fimreal_fimplan", round(sum(inicioreal_inicioplan),2) as "sum_inicioreal_inicioplan", ' + 
-        'round(sum(fimreal_inicioreal),2) as "sum_fimreal_inicioreal", round(sum(fimplan_inicioplan),2) as "sum_fimplan_inicioplan", ' +
-        'round(avg(fimreal_fimplan),2) as "avg_fimreal_fimplan", round(avg(inicioreal_inicioplan),2) as "avg_inicioreal_inicioplan", ' + 
-        'round(avg(fimreal_inicioreal),2) as "avg_fimreal_inicioreal", round(avg(fimplan_inicioplan),2) as "avg_fimplan_inicioplan" ' +
-        'from tbdados where range = ?',
-        [request.params.range],
-        (error, rows) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json(rows);
-        }
-    );
-};
-
-
-// retorna o somatório e média dos registros que possuem a linha fornecida
-const getStatsByLinha = (request, response) => {
-    bd.all(
-        'select linha, '+ 
-        'round(sum(fimreal_fimplan),2) as "sum_fimreal_fimplan", round(sum(inicioreal_inicioplan),2) as "sum_inicioreal_inicioplan", ' + 
-        'round(sum(fimreal_inicioreal),2) as "sum_fimreal_inicioreal", round(sum(fimplan_inicioplan),2) as "sum_fimplan_inicioplan", ' +
-        'round(avg(fimreal_fimplan),2) as "avg_fimreal_fimplan", round(avg(inicioreal_inicioplan),2) as "avg_inicioreal_inicioplan", ' + 
-        'round(avg(fimreal_inicioreal),2) as "avg_fimreal_inicioreal", round(avg(fimplan_inicioplan),2) as "avg_fimplan_inicioplan" ' +
-        'from tbdados where linha = ?',
-        [request.params.linha],
-        (error, rows) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json(rows);
-        }
-    );
-};
-
-// retorna o somatório e média dos registros que possuem o posto fornecido
-const getStatsByPosto = (request, response) => {
-    bd.all(
-        'select posto, '+ 
-        'round(sum(fimreal_fimplan),2) as "sum_fimreal_fimplan", round(sum(inicioreal_inicioplan),2) as "sum_inicioreal_inicioplan", ' + 
-        'round(sum(fimreal_inicioreal),2) as "sum_fimreal_inicioreal", round(sum(fimplan_inicioplan),2) as "sum_fimplan_inicioplan", ' +
-        'round(avg(fimreal_fimplan),2) as "avg_fimreal_fimplan", round(avg(inicioreal_inicioplan),2) as "avg_inicioreal_inicioplan", ' + 
-        'round(avg(fimreal_inicioreal),2) as "avg_fimreal_inicioreal", round(avg(fimplan_inicioplan),2) as "avg_fimplan_inicioplan" ' +
-        'from tbdados where posto = ?',
-        [request.params.posto],
-        (error, rows) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json(rows);
-        }
-    );
-};
-
-//retorna o somatório e média dos registros que possuem o range e posto fornecido
-const getStatsByRangePosto = (request, response) => {
-    bd.all(
-        'select range, posto, '+ 
-        'round(sum(fimreal_fimplan),2) as "sum_fimreal_fimplan", round(sum(inicioreal_inicioplan),2) as "sum_inicioreal_inicioplan", ' + 
-        'round(sum(fimreal_inicioreal),2) as "sum_fimreal_inicioreal", round(sum(fimplan_inicioplan),2) as "sum_fimplan_inicioplan", ' +
-        'round(avg(fimreal_fimplan),2) as "avg_fimreal_fimplan", round(avg(inicioreal_inicioplan),2) as "avg_inicioreal_inicioplan", ' + 
-        'round(avg(fimreal_inicioreal),2) as "avg_fimreal_inicioreal", round(avg(fimplan_inicioplan),2) as "avg_fimplan_inicioplan" ' +
-        'from tbdados where range = ? and posto = ?',
-        [request.params.range, request.params.posto],
-        (error, rows) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json(rows);
-        }
-    );
-};
-
-// retorna o somatório e média dos registros que possuem o range, linha e posto fornecido
-const getStatsByRangeLinhaPosto = (request, response) => {
-    bd.all(
-        'select range, linha, posto, '+ 
-        'round(sum(fimreal_fimplan),2) as "sum_fimreal_fimplan", round(sum(inicioreal_inicioplan),2) as "sum_inicioreal_inicioplan", ' + 
-        'round(sum(fimreal_inicioreal),2) as "sum_fimreal_inicioreal", round(sum(fimplan_inicioplan),2) as "sum_fimplan_inicioplan", ' +
-        'round(avg(fimreal_fimplan),2) as "avg_fimreal_fimplan", round(avg(inicioreal_inicioplan),2) as "avg_inicioreal_inicioplan", ' + 
-        'round(avg(fimreal_inicioreal),2) as "avg_fimreal_inicioreal", round(avg(fimplan_inicioplan),2) as "avg_fimplan_inicioplan" ' +
-        'from tbdados where range = ? and linha = ? and posto = ?',
-        [request.params.range, request.params.linha, request.params.posto],
-        (error, rows) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json(rows);
-        }
-    );
-};
-
-
 module.exports = {
-    /* LISTAR OS DADOS DISTINTOS */
-    getDistinctRange,
+    getLinhaGroupByPosto,
+    getLinhaRangeGroupByPosto,
+    getDistinctRangeByLinha,
     getDistinctLinha,
-    getDistinctPosto,
-
-    /* LISTAR OS DADOS */
-    getByRange,
-    getByLinha,
-    getByPosto,
-    getByRangePosto,
-    getByRangeLinhaPosto,
-
-    /* LISTAR AS ESTATÍSTICAS DOS DADOS */
-    getStatsByRange,
-    getStatsByLinha,
-    getStatsByPosto,
-    getStatsByRangePosto,
-    getStatsByRangeLinhaPosto
 };
--- server/src/updatecsv.js
+++ server/src/updatecsv.js
@@ -3,28 +3,28 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const diffdata = require('./diffdata');
 const fs = require('fs');
 const results = [];
-const inputfile = 'datas_06nov.csv'; /* precisa ser utf-8 sem BOM */
-const outputfile = 'datas_06nov - saida.csv';
+const inputfile = 'datas_14nov.csv'; /* precisa ser utf-8 sem BOM */
+const outputfile = 'datas_14nov - saida.csv';
 
 fs.createReadStream(inputfile)
     .pipe(csv({ separator: ';' }))
     .on('data', (data) => results.push(data))
     .on('end', () => {
         results.forEach(function (item) {
-            item.fimreal_fimplan = (item.datafimreal != 'NULL' && item.datafimplan != 'NULL') ?
-                diffdata.workedHours(new Date(item.datafimreal), new Date(item.datafimplan)) :
+            item.fimreal_fimplan = (item.DataFimReal != 'NULL' && item.DataFimPlan != 'NULL') ?
+                diffdata.intervalWorkedDays(new Date(item.DataFimReal), new Date(item.DataFimPlan)) :
                 'NULL';
 
-            item.inicioreal_inicioplan = (item.datainicioreal != 'NULL' && item.datainicioplan != 'NULL') ?
-                diffdata.workedHours(new Date(item.datainicioreal), new Date(item.datainicioplan)) :
+            item.inicioreal_inicioplan = (item.DataInicioReal != 'NULL' && item.DataInicioPlan != 'NULL') ?
+                diffdata.intervalWorkedDays(new Date(item.DataInicioReal), new Date(item.DataInicioPlan)) :
                 'NULL';
 
-            item.fimreal_inicioreal = (item.datafimreal != 'NULL' && item.datainicioreal != 'NULL') ?
-                diffdata.workedHours(new Date(item.datainicioreal), new Date(item.datafimreal)) :
+            item.fimreal_inicioreal = (item.DataFimReal != 'NULL' && item.DataInicioReal != 'NULL') ?
+                diffdata.workedDays(new Date(item.DataInicioReal), new Date(item.DataFimReal)) :
                 'NULL';
 
-            item.fimplan_inicioplan = (item.datafimplan != 'NULL' && item.datainicioplan != 'NULL') ?
-                diffdata.workedHours(new Date(item.datainicioplan), new Date(item.datafimplan)) :
+            item.fimplan_inicioplan = (item.DataFimPlan != 'NULL' && item.DataInicioPlan != 'NULL') ?
+                diffdata.workedDays(new Date(item.DataInicioPlan), new Date(item.DataFimPlan)) :
                 'NULL';
         });
         writeCSV(results);
@@ -37,14 +37,14 @@ const writeCSV = (data) => {
         path: outputfile,
         fieldDelimiter: ';',
         header: [
-            { id: 'range', title: 'range' },
-            { id: 'linha', title: 'linha' },
-            { id: 'posto', title: 'posto' },
-            { id: 'descricao', title: 'descricao' },
-            { id: 'datainicioplan', title: 'datainicioplan' },
-            { id: 'datafimplan', title: 'datafimplan' },
-            { id: 'datainicioreal', title: 'datainicioreal' },
-            { id: 'datafimreal', title: 'datafimreal' },
+            { id: 'Range', title: 'range' },
+            { id: 'Linha', title: 'linha' },
+            { id: 'Descricao', title: 'descricao' },
+            { id: 'Posto', title: 'posto' },
+            { id: 'DataInicioPlan', title: 'datainicioplan' },
+            { id: 'DataFimPlan', title: 'datafimplan' },
+            { id: 'DataInicioReal', title: 'datainicioreal' },
+            { id: 'DataFimReal', title: 'datafimreal' },
 
             { id: 'fimreal_fimplan', title: 'fimreal_fimplan' },
             { id: 'inicioreal_inicioplan', title: 'inicioreal_inicioplan' },
