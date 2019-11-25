466bd364f87472ee75f0f1f3f9523930584be198
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-11-25 10:47:47 -0300

    atualizando paths

--- client/src/router/paths.js
+++ client/src/router/paths.js
@@ -3,20 +3,24 @@ import Programa from '../views/Programa/Programa'
 import Slider from '../views/Slider/Slider'
 
 export default [
-	{
-		path:'/', 
-		component:Home,
-	},
-	{
-		path:'/programa', 
-		component:Programa,
-	},
-	{
-		path:'/slider', 
-		component:Slider,
-	},
-	{
-		path:'*',
-		redirect: '/'
-	}
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/programa',
+    component: Programa,
+  },
+  {
+    path: '/programa/:id',
+    component: Programa,
+  },
+  {
+    path: '/slider',
+    component: Slider,
+  },
+  {
+    path: '*',
+    redirect: '/',
+  },
 ]
