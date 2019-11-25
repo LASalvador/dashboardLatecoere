bb52266203ca5c24ac5113dd8b6ac84b0f2f33ed
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-11-25 10:22:06 -0300

    integrando front e back end no home

--- client/src/views/Home/Home.vue
+++ client/src/views/Home/Home.vue
@@ -1,19 +1,18 @@
 <template>
-	<v-container class="fluid">
-		<v-row>
-			<v-col 
-			v-for="i in 12"
-			:key="i"
-			cols="12"
-			md="3"
-			>
-				<card-image 
-					:programa="titulo"
-					:path="path"
-				/>
-			</v-col>
-		</v-row>
-	</v-container>
+  <v-container class="fluid">
+    <v-row>
+      <v-col
+        v-for="(programa, index) in programas"
+        :key="index"
+        cols="12"
+        md="3"
+      >
+        <card-image
+          :programa="programa.linha"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <style scoped>
 
--- client/src/views/Home/HomeController.js
+++ client/src/views/Home/HomeController.js
@@ -1,20 +1,15 @@
-import api  from '../../services/api'
+import api from '../../services/api'
 export default {
-	name: 'Home',
-	data() {
-		return {
-			titulo: 'Teste',
-			path: 'images/programas/ELP.png',
-			programas: [],
-		}
-	},
-	async beforeMount () {
-		
-	},
-	methods: {
-		getDados: async () => {
-			var programas = await api.distinctLinha()
-			this.programas = programas.data
-		}
-	}
+  name: 'Home',
+  data () {
+    return {
+      titulo: 'Teste',
+      path: 'images/programas/ELP.png',
+      programas: [],
+    }
+  },
+  async beforeMount () {
+    var programas = await api.distinctLinha()
+    this.programas = programas.data
+  },
 }
