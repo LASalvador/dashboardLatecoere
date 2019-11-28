09949c8ee8f49bd72ff904cba8bccaeb31105a14
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-11-28 05:05:58 -0300

    iniciando slider

--- client/src/views/Slider/Slider.vue
+++ client/src/views/Slider/Slider.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>home</div>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+      >
+        <v-carousel
+          height="1000"
+          interval="100"
+        >
+          <v-carousel-item
+            v-for="(item,i) in items"
+            :key="i"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          >
+            <v-row>
+              <v-col
+                cols="3"
+              >
+                <v-img
+                  v-if="item.path"
+                  :src="item.path"
+                  :alt="item.linha"
+                  max-height="200"
+                />
+              </v-col>
+              <v-col
+                v-for="(posto, index) in item.dados"
+                :key="index"
+                cols="3"
+              >
+                <v-card
+                  outlined
+                  :color="posto.somatorio === 0 ? 'green darken-3' : posto.somatorio > 0 ? 'light-blue  darken-3' : 'red darken-3' "
+                >
+                  <v-card-subtitle>
+                    Posto: {{ posto.posto }}
+                  </v-card-subtitle>
+                  <v-card-text class="white--text">
+                    <p>Duração Média Plan: {{ posto.duracaoMediaPlan }}</p>
+                    <p>Duração Média Real: {{ posto.duracaoMediaReal }}</p>
+                    <p>Somatório: {{ posto.somatorio }}</p>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <style scoped>
 </style>
--- client/src/views/Slider/SliderController.js
+++ client/src/views/Slider/SliderController.js
@@ -1,2 +1,66 @@
+import api from '../../services/api'
 export default {
+  name: 'Slider',
+  data () {
+    return {
+      items: [],
+      arquivo: '',
+    }
+  },
+  beforeMount () {
+    this.pegarDados()
+  },
+  beforeUpdate () {
+    console.log('aaaaaaaa')
+  },
+  methods: {
+    resolverCaminho (programa) {
+        var base = '/images/programas/'
+
+        if (programa === 'CF-1') {
+          this.arquivo = 'CF-1.png'
+        } else if (programa === 'CF-3') {
+          this.arquivo = 'CF-3.png'
+        } else if (programa === 'OW') {
+          this.arquivo = 'OW.png'
+        } else if (programa === 'Porta Bagag. Diant.E1' || programa === 'Porta Bagag. Diant.E2') {
+          this.arquivo = 'Porta Bag Dian E1 e E2.png'
+        } else if (programa === 'Porta Bagag. Tras.E1' || programa === 'Porta Bagag. Tras.E2') {
+          this.arquivo = 'Porta Bag Tras E1 e E2.png'
+        } else if (programa === 'Porta Pax Dian.') {
+          this.arquivo = 'Porta Pax Dian.png'
+        } else if (programa === 'Porta Pax Tras.') {
+          this.arquivo = 'Porta Pax Tras.png'
+        } else if (programa === 'Porta Serv. Dian.') {
+          this.arquivo = 'Porta Serv Dian.png'
+        } else if (programa === 'Porta Serv.Tras.') {
+          this.arquivo = 'Porta Serv Tras.png'
+        } else if (programa === 'Portas Legacy') {
+          this.arquivo = 'ELP.png'
+        } else {
+          this.arquivo = 'aaa.png'
+        }
+
+        return base + this.arquivo
+    },
+    async pegarDados () {
+      var programas = await api.distinctLinha()
+      programas.data.forEach(async (programa) => {
+        var progs = await api.getLinhaGroupByPosto(programa.linha)
+        var programasTemp = []
+        progs.data.forEach((posto) => {
+          programasTemp.push({
+            linha: posto.linha,
+            posto: posto.posto,
+            duracaoMediaReal: posto.avg_fimreal_inicioreal,
+            duracaoMediaPlan: posto.avg_fimplan_inicioplan,
+            somatorio: posto.sum_fimreal_fimplan,
+          })
+        })
+        programa.dados = programasTemp
+        programa.path = this.resolverCaminho(programa.linha)
+      })
+      this.items = programas.data
+    },
+  },
 }
