5d7b2f999b4b90dd7e33fce8eab5837873b09edd
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-11-29 09:25:34 -0300

    fix - atualizando views

--- client/src/views/Programa/Programa.vue
+++ client/src/views/Programa/Programa.vue
@@ -1,82 +1,66 @@
 <template>
   <v-container fluid>
-    <v-row align="center">
-      <v-col
-        cols="12"
-        md="6"
-        lg="6"
-        xl="6"
-      >
-        <v-card color="latecoereGray">
-          <v-card-title>
-            Programa: {{ programa }}
-          </v-card-title>
-          <v-card-subtitle>
-            Dados sobre:
-          </v-card-subtitle>
-          <v-card-text>
-            <v-row>
-              <v-col cols="6">
-                <v-select
-                  v-model="programa"
-                  :items="linhas"
-                  item-text="linha"
-                  item-value="linha"
-                  label="programas"
-                  outlined
-                  @change="atualizarLinha"
-                />
-            </v-col>
-            <v-col cols="6">
-              <v-select
-                v-model="range"
-                :items="ranges"
-                item-text="range"
-                item-value="range"
-                label="Ranges"
-                outlined
-                @change="pegarDadosRange"
-              />
-            </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col
-        cols="12"
-        md="6"
-        lg="6"
-        xl="6"
-      >
-        <img
-          v-if="path"
-          :src="path"
-          :alt="programa"
-          height="120"
-        >
-      </v-col>
-    </v-row>
     <v-row>
       <v-col
-        v-for="(posto, index) in programas"
-        :key="index"
         cols="12"
         md="3"
         lg="3"
         xl="3"
       >
-        <v-card
-          outlined
-          :color="posto.somatorio === 0 ? 'green darken-3' : posto.somatorio > 0 ? 'light-blue  darken-3' : 'red darken-3' "
-          height="100"
-        >
-          <v-card-text class="white--text">
-            <p class="subtitle-2">
-            Posto: {{ posto.posto }}
-            </p>
-            <p class="body-2">Total: {{ posto.somatorio }}</p>
+        <v-card color="latecoereGray">
+          <v-card-text>
+            <img
+              v-if="path"
+              :src="path"
+              :alt="programa"
+              height="120"
+            >
+            <v-select
+              v-model="programa"
+              :items="linhas"
+              item-text="linha"
+              item-value="linha"
+              label="Selecione uma linha"
+              outlined
+              @change="atualizarLinha"
+            />
+            <v-select
+              v-model="range"
+              :items="ranges"
+              item-text="range"
+              item-value="range"
+              label="Selecione um range"
+              outlined
+              @change="pegarDadosRange"
+            />
           </v-card-text>
         </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="9"
+        lg="9"
+        xl="9"
+      >
+        <v-row dense>
+          <v-col
+            v-for="(posto, index) in programas"
+            :key="index"
+            cols="2"
+          >
+            <v-card
+              outlined
+              :color="posto.somatorio === 0 ? 'green darken-3' : posto.somatorio > 0 ? 'light-blue  darken-3' : 'red darken-3' "
+              height="115"
+            >
+              <v-card-text class="white--text">
+                <span class="body-2">
+                  {{ posto.posto }}
+                </span>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
--- client/src/views/Slider/Slider.vue
+++ client/src/views/Slider/Slider.vue
@@ -1,70 +1,19 @@
 <template>
   <v-container fluid>
-    <v-row>
+    <v-row dense>
       <v-col
-        cols="12"
-      >
-        <v-sheet
-          class="mx-auto"
-          elevation="8"
-          max-width="800"
-        >
-          <v-slide-group
-            v-model="model"
-            class="pa-4"
-            center-active
-            show-arrows
-          >
-            <v-slide-item
-              v-for="(img, index) in imagens"
-              :key="index"
-              v-slot:default="{ active, toggle }"
-            >
-              <v-card
-                :color="active ? 'latecoereBlueDark' : 'grey lighten-5'"
-                class="ma-4"
-                height="180"
-                width="190"
-                @click="toggle"
-              >
-                <v-row
-                  class="fill-height"
-                  align="center"
-                  justify="center"
-                >
-                  <img
-                    class="align-end"
-                    height="150px"
-                    :src="img.path"
-                    :alt="img.title"
-                  >
-                  <v-scale-transition />
-                </v-row>
-              </v-card>
-            </v-slide-item>
-          </v-slide-group>
-        </v-sheet>
-      </v-col>
-      <v-col
-        v-for="(posto, index) in items"
-        :key="index"
         cols="12"
         md="3"
         lg="3"
         xl="3"
       >
-        <v-card
-          outlined
-          :color="posto.somatorio === 0 ? 'green darken-3' : posto.somatorio > 0 ? 'light-blue  darken-3' : 'red darken-3' "
-          height="100"
-        >
-          <v-card-text class="white--text">
-            <p class="subtitle-2">
-            Posto: {{ posto.posto }}
-            </p>
-            <p class="body-2">Total: {{ posto.somatorio }}</p>
-          </v-card-text>
-        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="9"
+        lg="9"
+        xl="9"
+      >
       </v-col>
     </v-row>
   </v-container>
