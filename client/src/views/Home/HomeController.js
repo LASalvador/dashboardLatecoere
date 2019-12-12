3f850dc6141bb7cc0b4a87d98b2eca09fbc5bf5f
Lucas Salvador <lucas@superclient.com.br>
2019-12-11 23:41:06 -0300

    atualizando o controller

--- client/src/views/Home/HomeController.js
+++ client/src/views/Home/HomeController.js
@@ -1,15 +1,7 @@
-import api from '../../services/api'
 export default {
   name: 'Home',
   data () {
     return {
-      titulo: 'Teste',
-      path: 'images/programas/ELP.png',
-      programas: [],
     }
-  },
-  async beforeMount () {
-    var programas = await api.distinctLinha()
-    this.programas = programas.data
   },
 }
